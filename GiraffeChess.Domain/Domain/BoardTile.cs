b478e889c1c14b04f2672325b031174dccef84fd
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-02 14:34:01 +0100

    Add domain classes

---
+++ GiraffeChess.Domain/Domain/BoardTile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiraffeChess.Domain.Domain
+{
+    public class BoardTile
+    {
+    }
+}
---
+++ GiraffeChess.Domain/Domain/Side.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiraffeChess.Domain.Domain
+{
+    public enum Side
+    {
+        White,
+        Black
+    }
+}
